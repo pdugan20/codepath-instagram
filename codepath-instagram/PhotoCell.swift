f3dccac197b424049572ec9d2dd8675748bb28f6
Patrick Dugan <dugan.pat@gmail.com>
2015-06-09 21:33:15 -0700

    working url implementation
    urls for photos implemented correctly into tableview cells

---
+++ codepath-instagram/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  codepath-instagram
+//
+//  Created by Patrick Dugan on 6/9/15.
+//  Copyright (c) 2015 Daily Doog Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var instaUrlLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- codepath-instagram/PhotosViewController.swift
+++ codepath-instagram/PhotosViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var feedTableView: UITableView!
+    
+    var photos: NSArray! = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +20,29 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         feedTableView.delegate = self
         feedTableView.dataSource = self
         
+        var clientId = "a3fe963389494f199aa550a2dbf55e73"
+        var instaUrl = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
+        var request = NSURLRequest(URL: instaUrl)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
+            (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
+            self.photos = responseDictionary["data"] as! NSArray
+            self.feedTableView.reloadData()
+            // println("response: \(self.photos)")
+        }
+        
     }
     
     // How many rows in TableView
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return photos.count
     }
     
     // View for the cell in the TableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = feedTableView.dequeueReusableCellWithIdentifier("PhotoCell") as UITableViewCell
+        var cell = feedTableView.dequeueReusableCellWithIdentifier("PhotoCell") as! PhotoCell
+        var photo = photos[indexPath.row] as! NSDictionary
+        cell.instaUrlLabel.text = photo.valueForKeyPath("images.low_resolution.url") as? String
         return cell
     }
 
